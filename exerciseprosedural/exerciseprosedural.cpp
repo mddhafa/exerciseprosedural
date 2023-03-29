d2496e3582e37059a14e1b1532c99fc45dd31cd7
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-03-29 10:42:44 +0700

    masukkan void input,display dan jmlhkandidat

--- exerciseprosedural/exerciseprosedural.cpp
+++ exerciseprosedural/exerciseprosedural.cpp
@@ -14,5 +14,38 @@ int nilM[2], nilB[2];
 float nilRerata[2];
 string nama[2];
 
+void input() {
+	for (int i = 0; i < 20; i++) {
+		cout << "masukkan nama kandidat : ";
+		cin >> nama[i];
+		cout << "masukkan nilai Matematika=";
+		cin >> nilM[i];
+		cout << "masukkan nilai Bahasa Inggris=";
+		cin >> nilB[i];
+
+		nilRerata[i] = rerata(nilM[i], nilB[i]);
+	}
+}
+
+void display() {
+	cout << "Tampilkan nama kandidat \t\ status : " << endl;
+	for (int i = 0; i < 20; i++)
+		cout << nama[i] << "\t\t" << status(nilM[i], nilB[i]);
+}
+
+void jmlhkandidat() {
+	int jumlahditerima = 0;
+	int jumlahditolak = 0;
+	for (int i = 0; i < 20; i++) {
+		if (status(nilRerata[i], nilM[i]) == "diterima") {
+			jumlahditerima++;
+		}
+		else {
+			jumlahditolak++;
+		}
+	}
+	cout << "jumlah kandidat diterima" << endl;
+	cout << "jumlah kandidat ditolak" << endl;
+}
 
 
