9cbc114ee5fec4e5201cc5efd55f2827debdfb41
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-03-29 10:41:02 +0700

    masukkan string status

--- exerciseprosedural/exerciseprosedural.cpp
+++ exerciseprosedural/exerciseprosedural.cpp
@@ -4,3 +4,10 @@ using namespace std;
 double rerata(double a, double b) {
 	return (a + b) / 2;
 }
+string status(double r, double n) {
+	if (r >= 70 and n >= 80)
+		return "diterima";
+	else
+		return "ditolak";
+}
+
